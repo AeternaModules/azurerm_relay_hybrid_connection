91c32f3ef71ed0b338d47294ed24ae93d903b157
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "relay_hybrid_connections" {
-  description = "All relay_hybrid_connection resources"
-  value       = azurerm_relay_hybrid_connection.relay_hybrid_connections
-}
 output "relay_hybrid_connections_name" {
-  description = "List of name values across all relay_hybrid_connections"
-  value       = [for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : v.name]
+  description = "Map of name values across all relay_hybrid_connections, keyed the same as var.relay_hybrid_connections"
+  value       = { for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : k => v.name }
 }
 output "relay_hybrid_connections_relay_namespace_name" {
-  description = "List of relay_namespace_name values across all relay_hybrid_connections"
-  value       = [for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : v.relay_namespace_name]
+  description = "Map of relay_namespace_name values across all relay_hybrid_connections, keyed the same as var.relay_hybrid_connections"
+  value       = { for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : k => v.relay_namespace_name }
 }
 output "relay_hybrid_connections_requires_client_authorization" {
-  description = "List of requires_client_authorization values across all relay_hybrid_connections"
-  value       = [for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : v.requires_client_authorization]
+  description = "Map of requires_client_authorization values across all relay_hybrid_connections, keyed the same as var.relay_hybrid_connections"
+  value       = { for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : k => v.requires_client_authorization }
 }
 output "relay_hybrid_connections_resource_group_name" {
-  description = "List of resource_group_name values across all relay_hybrid_connections"
-  value       = [for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : v.resource_group_name]
+  description = "Map of resource_group_name values across all relay_hybrid_connections, keyed the same as var.relay_hybrid_connections"
+  value       = { for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : k => v.resource_group_name }
 }
 output "relay_hybrid_connections_user_metadata" {
-  description = "List of user_metadata values across all relay_hybrid_connections"
-  value       = [for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : v.user_metadata]
+  description = "Map of user_metadata values across all relay_hybrid_connections, keyed the same as var.relay_hybrid_connections"
+  value       = { for k, v in azurerm_relay_hybrid_connection.relay_hybrid_connections : k => v.user_metadata }
 }
 
